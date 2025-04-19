4f2c87997977e17a2f8729ae07fdbcb42561fa23
Iván Bellón <Ivbegon04@gmail.com>
2025-04-19 19:00:37 +0200

    Cambios
    Actualiza Visual Studio y añade proyecto "entidades"
    
    Se ha actualizado la versión de Visual Studio en `Farmacia.sln` a 17.13.35806.99.
    Se ha añadido un nuevo proyecto llamado "entidades" con configuraciones de compilación correspondientes.
    Se han creado las clases `DetalleVenta`, `Empleado`, `Medicamento`, `Proveedor` y `Venta` en el espacio de nombres `entidades`, cada una con sus propiedades privadas.
    Además, se ha añadido el archivo de proyecto `entidades.csproj` con el SDK de .NET y el marco de destino `net8.0`.

---
+++ entidades/Medicamento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entidades
+{
+    public class Medicamento
+    {
+        private int IdMedicamento { get; set; }
+        private string Nombre { get; set; }
+        private string Descripcion { get; set; }
+        private decimal Precio { get; set; }
+        private int Cantidad { get; set; }
+        private DateTime FechaVencimiento { get; set; }
+        private bool RequiereReceta { get; set; }
+        private int IdProveedor { get; set; }
+
+    }
+}
---
+++ entidades/Proveedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entidades
+{
+    public class Proveedor
+    {
+        private int IdProveedor { get; set; }
+        private string Nombre { get; set; }
+        private string Telefono { get; set; }
+        private string Direccion { get; set; }
+        private string Email { get; set; }
+        private string Cif { get; set; }
+
+    }
+}
