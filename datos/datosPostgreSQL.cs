dbf0d7146e44c2658ca761c7f4c716f0ac3301bb
Iván Ortega <ivan83raquel@gmail.com>
2025-04-19 18:39:01 +0200

    Añadir proyecto "datos" y clase de conexión PostgreSQL
    Se ha añadido un nuevo proyecto llamado "datos" al archivo de solución de Visual Studio, con configuraciones para Debug y Release. Se ha creado el archivo `datos.csproj` utilizando el SDK de .NET y especificando el marco de trabajo `net8.0`, además de añadir referencias a los paquetes `Npgsql` y `Npgsql.EntityFrameworkCore.PostgreSQL`. También se ha introducido la clase `datosPostgreSQL`, que gestiona la conexión a una base de datos PostgreSQL y proporciona métodos para manejar transacciones y ejecutar consultas.

---
+++ datos/datosPostgreSQL.cs
@@ -0,0 +1,108 @@
+﻿using Npgsql;
+using System.Data;
+using System.Collections.Generic;
+
+namespace datos
+{
+    public class datosPostgreSQL
+    {
+        #region Variables y Propiedades
+        protected NpgsqlConnection MConexion;
+        protected string MCadenaConexion;
+        protected NpgsqlTransaction MTransaccion;
+        protected bool EnTransaccion;
+
+        protected NpgsqlConnection Conexion
+        {
+            get
+            {
+                if (MConexion == null)
+                    MConexion = CrearConexion();
+
+                if (MConexion.State != ConnectionState.Open)
+                    MConexion.Open();
+
+                return MConexion;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public datosPostgreSQL()
+        {
+            MCadenaConexion = "Server=localhost;Database=farmacia;User ID=postgres;Password=postgres;";
+        }
+        #endregion
+
+        #region Métodos Básicos
+        protected NpgsqlConnection CrearConexion()
+        {
+            return new NpgsqlConnection(MCadenaConexion);
+        }
+
+        protected NpgsqlCommand CrearComando(string consulta, CommandType tipo = CommandType.Text)
+        {
+            NpgsqlCommand comando = new NpgsqlCommand(consulta, Conexion);
+            comando.CommandType = tipo;
+            if (EnTransaccion)
+                comando.Transaction = MTransaccion;
+            return comando;
+        }
+
+        protected void AgregarParametro(NpgsqlCommand comando, string nombre, object valor, NpgsqlTypes.NpgsqlDbType tipo)
+        {
+            NpgsqlParameter parametro = new NpgsqlParameter(nombre, tipo);
+            parametro.Value = valor ?? DBNull.Value;
+            comando.Parameters.Add(parametro);
+        }
+
+        public void IniciarTransaccion()
+        {
+            MTransaccion = Conexion.BeginTransaction();
+            EnTransaccion = true;
+        }
+
+        public void ConfirmarTransaccion()
+        {
+            MTransaccion.Commit();
+            EnTransaccion = false;
+        }
+
+        public void CancelarTransaccion()
+        {
+            MTransaccion.Rollback();
+            EnTransaccion = false;
+        }
+        #endregion
+
+        #region Métodos de Ejecución
+        public int EjecutarComando(NpgsqlCommand comando)
+        {
+            return comando.ExecuteNonQuery();
+        }
+
+        public object EjecutarEscalar(NpgsqlCommand comando)
+        {
+            return comando.ExecuteScalar();
+        }
+
+        public NpgsqlDataReader EjecutarLector(NpgsqlCommand comando)
+        {
+            return comando.ExecuteReader();
+        }
+
+        public DataTable EjecutarConsulta(string consulta)
+        {
+            DataTable tabla = new DataTable();
+            using (NpgsqlCommand comando = CrearComando(consulta))
+            {
+                using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
+                {
+                    adaptador.Fill(tabla);
+                }
+            }
+            return tabla;
+        }
+        #endregion
+    }
+}
