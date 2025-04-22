3eb2f8c7a3195f9017c8adc553502c72278984f2
Iván Ortega <ivan83raquel@gmail.com>
2025-04-22 17:32:22 +0200

    Añadir controles y funcionalidades a frmVentas
    Se han añadido controles como ComboBox, NumericUpDown, y DataGridView en `frmVentas.Designer.cs` para gestionar la selección de medicamentos y detalles de venta. En `frmVentas.cs`, se han implementado nuevas funcionalidades para cargar medicamentos, agregar detalles de venta, calcular el total y registrar la venta, optimizando el código y eliminando redundancias.

--- Farmacia/frmVentas.Designer.cs
+++ Farmacia/frmVentas.Designer.cs
@@ -26,86 +26,112 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
+        private System.Windows.Forms.ComboBox cmbMedicamento;
+        private System.Windows.Forms.NumericUpDown nudCantidad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgvDetalleVenta;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtRecibido;
+        private System.Windows.Forms.Button btnRegistrarVenta;
+        private System.Windows.Forms.DataGridView dgvVentas;
+        private System.Windows.Forms.Button btnVerFactura;
+        private System.Windows.Forms.Label lblRecibido;
+
         private void InitializeComponent()
         {
-            lbl_titulo = new Label();
-            grpBoxBusqueda = new GroupBox();
-            lbl_medicamento = new Label();
-            txtBusqueda = new TextBox();
-            btn_Buscar = new Button();
-            grpBoxBusqueda.SuspendLayout();
-            SuspendLayout();
-            // 
-            // lbl_titulo
-            // 
-            lbl_titulo.AutoSize = true;
-            lbl_titulo.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl_titulo.Location = new Point(800, 49);
-            lbl_titulo.Name = "lbl_titulo";
-            lbl_titulo.Size = new Size(104, 38);
-            lbl_titulo.TabIndex = 0;
-            lbl_titulo.Text = "Ventas";
-            // 
-            // grpBoxBusqueda
-            // 
-            grpBoxBusqueda.BackColor = Color.DarkSeaGreen;
-            grpBoxBusqueda.Controls.Add(btn_Buscar);
-            grpBoxBusqueda.Controls.Add(txtBusqueda);
-            grpBoxBusqueda.Controls.Add(lbl_medicamento);
-            grpBoxBusqueda.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            grpBoxBusqueda.Location = new Point(55, 146);
-            grpBoxBusqueda.Name = "grpBoxBusqueda";
-            grpBoxBusqueda.Size = new Size(1078, 379);
-            grpBoxBusqueda.TabIndex = 1;
-            grpBoxBusqueda.TabStop = false;
-            grpBoxBusqueda.Text = "Busqueda de medicamento";
-            // 
-            // lbl_medicamento
-            // 
-            lbl_medicamento.AutoSize = true;
-            lbl_medicamento.Location = new Point(22, 57);
-            lbl_medicamento.Name = "lbl_medicamento";
-            lbl_medicamento.Size = new Size(185, 32);
-            lbl_medicamento.TabIndex = 2;
-            lbl_medicamento.Text = "Medicamento :";
-            // 
-            // txtBusqueda
-            // 
-            txtBusqueda.BackColor = Color.Silver;
-            txtBusqueda.BorderStyle = BorderStyle.FixedSingle;
-            txtBusqueda.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtBusqueda.ForeColor = SystemColors.HighlightText;
-            txtBusqueda.Location = new Point(244, 54);
-            txtBusqueda.Name = "txtBusqueda";
-            txtBusqueda.PlaceholderText = "Busqueda";
-            txtBusqueda.Size = new Size(561, 40);
-            txtBusqueda.TabIndex = 3;
-            // 
-            // btn_Buscar
-            // 
-            btn_Buscar.BackColor = Color.Black;
-            btn_Buscar.ForeColor = Color.White;
-            btn_Buscar.Location = new Point(859, 54);
-            btn_Buscar.Name = "btn_Buscar";
-            btn_Buscar.Size = new Size(112, 40);
-            btn_Buscar.TabIndex = 4;
-            btn_Buscar.Text = "Buscar";
-            btn_Buscar.UseVisualStyleBackColor = false;
-            // 
+            this.cmbMedicamento = new System.Windows.Forms.ComboBox();
+            this.nudCantidad = new System.Windows.Forms.NumericUpDown();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgvDetalleVenta = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtRecibido = new System.Windows.Forms.TextBox();
+            this.btnRegistrarVenta = new System.Windows.Forms.Button();
+            this.dgvVentas = new System.Windows.Forms.DataGridView();
+            this.btnVerFactura = new System.Windows.Forms.Button();
+            this.lblRecibido = new System.Windows.Forms.Label();
+
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleVenta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).BeginInit();
+
+            this.SuspendLayout();
+
+            // cmbMedicamento
+            this.cmbMedicamento.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.cmbMedicamento.Location = new System.Drawing.Point(20, 20);
+            this.cmbMedicamento.Size = new System.Drawing.Size(200, 25);
+
+            // nudCantidad
+            this.nudCantidad.Location = new System.Drawing.Point(230, 20);
+            this.nudCantidad.Minimum = 1;
+            this.nudCantidad.Value = 1;
+            this.nudCantidad.Size = new System.Drawing.Size(60, 25);
+
+            // btnAgregar
+            this.btnAgregar.Location = new System.Drawing.Point(300, 20);
+            this.btnAgregar.Size = new System.Drawing.Size(100, 25);
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+
+            // dgvDetalleVenta
+            this.dgvDetalleVenta.Location = new System.Drawing.Point(20, 60);
+            this.dgvDetalleVenta.Size = new System.Drawing.Size(380, 120);
+            this.dgvDetalleVenta.ReadOnly = true;
+            this.dgvDetalleVenta.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // lblTotal
+            this.lblTotal.Location = new System.Drawing.Point(20, 190);
+            this.lblTotal.Size = new System.Drawing.Size(200, 25);
+            this.lblTotal.Text = "Total: $0.00";
+
+            // lblRecibido
+            this.lblRecibido.Location = new System.Drawing.Point(20, 220);
+            this.lblRecibido.Text = "Monto recibido:";
+            this.lblRecibido.Size = new System.Drawing.Size(120, 25);
+
+            // txtRecibido
+            this.txtRecibido.Location = new System.Drawing.Point(140, 220);
+            this.txtRecibido.Size = new System.Drawing.Size(100, 25);
+
+            // btnRegistrarVenta
+            this.btnRegistrarVenta.Location = new System.Drawing.Point(260, 220);
+            this.btnRegistrarVenta.Size = new System.Drawing.Size(140, 25);
+            this.btnRegistrarVenta.Text = "Registrar Venta";
+            this.btnRegistrarVenta.Click += new System.EventHandler(this.btnRegistrarVenta_Click);
+
+            // dgvVentas
+            this.dgvVentas.Location = new System.Drawing.Point(20, 260);
+            this.dgvVentas.Size = new System.Drawing.Size(500, 150);
+            this.dgvVentas.ReadOnly = true;
+            this.dgvVentas.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // btnVerFactura
+            this.btnVerFactura.Location = new System.Drawing.Point(20, 420);
+            this.btnVerFactura.Size = new System.Drawing.Size(150, 25);
+            this.btnVerFactura.Text = "Ver Factura";
+            this.btnVerFactura.Click += new System.EventHandler(this.btnVerFactura_Click);
+
             // frmVentas
-            // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1750, 1002);
-            Controls.Add(grpBoxBusqueda);
-            Controls.Add(lbl_titulo);
-            Name = "frmVentas";
-            Text = "frmVentas";
-            grpBoxBusqueda.ResumeLayout(false);
-            grpBoxBusqueda.PerformLayout();
-            ResumeLayout(false);
-            PerformLayout();
+            this.ClientSize = new System.Drawing.Size(550, 460);
+            this.Controls.Add(this.cmbMedicamento);
+            this.Controls.Add(this.nudCantidad);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvDetalleVenta);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblRecibido);
+            this.Controls.Add(this.txtRecibido);
+            this.Controls.Add(this.btnRegistrarVenta);
+            this.Controls.Add(this.dgvVentas);
+            this.Controls.Add(this.btnVerFactura);
+            this.Text = "Gestión de Ventas";
+
+            ((System.ComponentModel.ISupportInitialize)(this.nudCantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleVenta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVentas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
 
         #endregion
 
--- Farmacia/frmVentas.cs
+++ Farmacia/frmVentas.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using entidades;
+using Negocio.Medicamentos;
+using Negocio.Ventas;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +13,97 @@ using System.Windows.Forms;
 
 namespace Farmacia
 {
-    public partial class frmVentas: Form
+    public partial class frmVentas : Form
     {
+        private List<DetalleVenta> detalles = new List<DetalleVenta>();
+        private VentasNegocio ventasNegocio = new VentasNegocio();
+        private MedicamentosNegocio medicamentosNegocio = new MedicamentosNegocio();
+
         public frmVentas()
         {
             InitializeComponent();
+            CargarMedicamentos();
+            ListarVentas();
+        }
+
+        private void CargarMedicamentos()
+        {
+            var medicamentos = medicamentosNegocio.ListarMedicamentos();
+            cmbMedicamento.DataSource = medicamentos;
+            cmbMedicamento.DisplayMember = "Nombre";
+            cmbMedicamento.ValueMember = "IdMedicamento";
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            var seleccionado = (Medicamento)cmbMedicamento.SelectedItem;
+            int cantidad = (int)nudCantidad.Value;
+
+            if (cantidad > seleccionado.Cantidad)
+            {
+                MessageBox.Show("No hay suficiente stock.");
+                return;
+            }
+
+            detalles.Add(new DetalleVenta
+            {
+                IdMedicamento = seleccionado.IdMedicamento,
+                Cantidad = cantidad
+            });
+
+            dgvDetalleVenta.DataSource = null;
+            dgvDetalleVenta.DataSource = detalles;
+            CalcularTotal();
+        }
+
+        private void CalcularTotal()
+        {
+            decimal total = 0;
+            foreach (var det in detalles)
+            {
+                var med = medicamentosNegocio.ObtenerMedicamento(det.IdMedicamento);
+                total += med.Precio * det.Cantidad;
+            }
+            lblTotal.Text = $"Total: {total:C}";
+        }
+
+        private void btnRegistrarVenta_Click(object sender, EventArgs e)
+        {
+            decimal montoRecibido = decimal.Parse(txtRecibido.Text);
+            decimal total = detalles.Sum(d =>
+            {
+                var med = medicamentosNegocio.ObtenerMedicamento(d.IdMedicamento);
+                return med.Precio * d.Cantidad;
+            });
+
+            Venta venta = new Venta
+            {
+                IdEmpleado = 1,
+                Vuelto = montoRecibido,
+                Total = total
+            };
+
+            int idVenta = ventasNegocio.RegistrarVenta(venta, detalles);
+            MessageBox.Show($"Venta registrada con ID {idVenta}");
+            detalles.Clear();
+            dgvDetalleVenta.DataSource = null;
+            ListarVentas();
+        }
+
+        private void ListarVentas()
+        {
+            dgvVentas.DataSource = ventasNegocio.ListarVentas();
+        }
+
+        private void btnVerFactura_Click(object sender, EventArgs e)
+        {
+            if (dgvVentas.CurrentRow != null)
+            {
+                int idVenta = (int)dgvVentas.CurrentRow.Cells["IdVenta"].Value;
+                var venta = ventasNegocio.ObtenerVenta(idVenta);
+                MessageBox.Show($"Factura de Venta {idVenta}:\nTotal: {venta.Total:C}\nVuelto: {venta.Vuelto:C}");
+            }
         }
     }
+
 }
