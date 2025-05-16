c7d7a5a1bf1b8c55929b0017f5b8020c4f9f823e
Iván Bellón <Ivbegon04@gmail.com>
2025-05-16 23:16:25 +0200

    Actualización del conjunto de datos y diseño de informes
    Se han realizado cambios significativos en la estructura y diseño del conjunto de datos en el proyecto de farmacia. Se ha añadido un código extenso en `DatosFarmacia.Designer.cs` para definir un conjunto de datos fuertemente tipado y se han actualizado los archivos XML relacionados (`.xsc`, `.xsd`, `.xss`) para reflejar la nueva configuración y diseño.
    
    Además, se han eliminado referencias a paquetes de FastReport en `Farmacia.csproj`, indicando un cambio en la gestión de informes. Se han modificado los informes `facturaReport.rdlc` e `informeVentas.rdlc` para incluir nuevos elementos de diseño y campos relevantes, y se ha añadido una instancia de `Pruebaxsd` en `frmInformes.Designer.cs` para utilizar el nuevo conjunto de datos.

--- Farmacia/frmInformes.Designer.cs
+++ Farmacia/frmInformes.Designer.cs
@@ -28,7 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pruebaxsd1 = new Pruebaxsd();
+            ((System.ComponentModel.ISupportInitialize)pruebaxsd1).BeginInit();
             SuspendLayout();
+            // 
+            // pruebaxsd1
+            // 
+            pruebaxsd1.DataSetName = "Pruebaxsd";
+            pruebaxsd1.Namespace = "http://tempuri.org/Pruebaxsd.xsd";
+            pruebaxsd1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // frmInformes
             // 
@@ -38,10 +46,12 @@
             Name = "frmInformes";
             Text = "FormularioInforme";
             Load += frmInformes_Load;
+            ((System.ComponentModel.ISupportInitialize)pruebaxsd1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
+        private Pruebaxsd pruebaxsd1;
     }
 }
